c5038a0cb212f75e3a6480204d584efc8308700e
User <chl8263@naver.com>
2017-11-22 23:23:49 +0900

    fragment속도개선

--- app/src/main/java/com/example/user/with_family/MainActivity.java
+++ app/src/main/java/com/example/user/with_family/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.with_family;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -16,7 +17,14 @@ import com.example.user.with_family.ui.f_chat.Chat_Fragment;
 import com.example.user.with_family.ui.f_control.ControlFragment;
 import com.example.user.with_family.ui.f_home.HomeFragment;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private Album_Fragment albumFragment;
+    private Calendar_Fragment calendarFragment;
+    private Chat_Fragment chatFragment;
+    private ControlFragment controlFragment;
+    private HomeFragment homeFragment;
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener;
 
     @Override
@@ -25,31 +33,32 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         init();
-        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        initView();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
     }
-    public void init(){
-        selectNavigationView(HomeFragment.newInstance());
+
+    public void init() {
         onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.navigation_home:
-                        selectNavigationView(HomeFragment.newInstance());
+                        firstSelectNavView("home");
                         return true;
                     case R.id.navigation_album:
-                        selectNavigationView(Album_Fragment.newInstance());
+                        firstSelectNavView("album");
                         return true;
                     case R.id.navigation_chat:
-                        selectNavigationView(Chat_Fragment.newInstance());
+                        firstSelectNavView("chat");
                         return true;
                     case R.id.navigation_calendar:
-                        selectNavigationView(Calendar_Fragment.newInstance());
+                        firstSelectNavView("calendar");
                         return true;
                     case R.id.navigation_control:
-                        selectNavigationView(ControlFragment.newInstance());
+                        firstSelectNavView("control");
                         return true;
                 }
                 return false;
@@ -57,7 +66,47 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    private void selectNavigationView(Fragment fragment) {
+    public void initView() {
+        albumFragment = Album_Fragment.newInstance();
+        calendarFragment = Calendar_Fragment.newInstance();
+        chatFragment = Chat_Fragment.newInstance();
+        controlFragment = ControlFragment.newInstance();
+        homeFragment = HomeFragment.newInstance();
+        initNavView();
+    }
+
+    private void initNavView() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment, homeFragment, "home");
+        fragmentTransaction.add(R.id.fragment, albumFragment, "album");
+        fragmentTransaction.add(R.id.fragment, chatFragment, "chat");
+        fragmentTransaction.add(R.id.fragment, calendarFragment, "calendar");
+        fragmentTransaction.add(R.id.fragment, controlFragment, "control");
+        fragmentTransaction.hide(albumFragment);
+        fragmentTransaction.hide(chatFragment);
+        fragmentTransaction.hide(calendarFragment);
+        fragmentTransaction.hide(controlFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void firstSelectNavView(String tag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        @SuppressLint("RestrictedApi") List<Fragment> fragments = fragmentManager.getFragments();
+        for (Fragment fragment : fragments) {
+            if (fragment.getTag().equals(tag)) {
+                fragmentTransaction.show(fragment);
+            } else {
+                fragmentTransaction.hide(fragment);
+            }
+        }
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+
+    private void selectNavView(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
